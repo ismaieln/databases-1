891b22257da8369d49ca32e3f98a8ffb73de1a43
ismaiel <ismaiel.nab@gmail.com>
2020-12-25 17:58:25 +0100

    rectify the excersices

--- Week1/excercises/app.js
+++ Week1/excercises/app.js
@@ -3,9 +3,9 @@ const mysql = require("mysql");
 // Make the connection
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "Omran2003",
-  database: "meetup",
+  user: "hyfuser",
+  password: "hyfpassword",
+  // database: "meetup",
 });
 
 // connect
@@ -31,72 +31,63 @@ const room =
 doQuery(room);
 
 // create Meeting table
-meeting =
+const meeting =
   "CREATE TABLE IF NOT EXISTS Meeting(meeting_no INT , meeting_title VARCHAR(200), starting_time DATETIME, ending_time  DATETIME, room_no INT ) ";
 doQuery(meeting);
 
 // insert to the invitee table
-insertDataToTable = function () {
-  let invite = [
-    [1, "Ismaiel", "Alnabwani"],
-    [2, "Omran", "Alnabwani"],
-    [3, "Salim", "Janbih"],
-    [4, "Walid", "Hasson"],
-    [5, "Hassan", "Abas"],
-  ];
-  let sql =
-    "INSERT INTO Invitee (invitee_no  , invitee_name , invited_by ) VALUES ?";
-  let query = db.query(sql, [invite], (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(`Invitations sent... ${result}`);
-  });
-};
+const invite = [
+  [1, "Ismaiel", "Alnabwani"],
+  [2, "Omran", "Alnabwani"],
+  [3, "Salim", "Janbih"],
+  [4, "Walid", "Hasson"],
+  [5, "Hassan", "Abas"],
+];
+const sqlInvit =
+  "INSERT IGNORE INTO Invitee (invitee_no  , invitee_name , invited_by ) VALUES ?";
+insertValues(sqlInvit, invite);
 
 // insert to the room table
-const insertValueToRoomTable = function () {
-  let rooms = [
-    [1, "confirnce", 10],
-    [2, "Skyroom", 50],
-    [3, "heaven", 8],
-    [4, "sport", 22],
-    [5, "suite", 40],
-  ];
-  let sql = "INSERT INTO Room (room_no , room_name , floor_number ) VALUES ?";
-  let query = db.query(sql, [rooms], (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(`room records inserted... ${result}`);
-  });
-};
+const rooms = [
+  [1, "confirnce", 10],
+  [2, "Skyroom", 50],
+  [3, "heaven", 8],
+  [4, "sport", 22],
+  [5, "suite", 40],
+];
+const sqlRoom =
+  "INSERT IGNORE INTO Room (room_no , room_name , floor_number ) VALUES ?";
+insertValues(sqlRoom, rooms);
 
 // insert to the meeting table
-const insertValueToMeetingTable = function () {
-  let meeting = [
-    [11, "tranning", "2020-04-06 10:10:23", "2020-04-06 11:10:23", 12],
-    [14, " intro", "2020-07-07 14:10:00", "2020-07-07 16:10:00", 40],
-    [29, "breafing", "2020-08-02 10:40:00", "2020-08-02 13:40:00", 3],
-    [30, "consolting", "2020-09-07 13:00:00", "2020-09-07 15:00:00", 5],
-    [58, "cebration", "2020-11-07 09:10:00", "2020-11-07 12:10:00", 9],
-  ];
-  let sql =
-    "INSERT INTO Meeting (meeting_no , meeting_title , starting_time , ending_time , room_no ) VALUES ?";
-  let query = db.query(sql, [meeting], (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(`meeting records inserted... ${result} `);
-  });
-};
+const meetings = [
+  [11, "tranning", "2020-04-06 10:10:23", "2020-04-06 11:10:23", 12],
+  [14, " intro", "2020-07-07 14:10:00", "2020-07-07 16:10:00", 40],
+  [29, "breafing", "2020-08-02 10:40:00", "2020-08-02 13:40:00", 3],
+  [30, "consolting", "2020-09-07 13:00:00", "2020-09-07 15:00:00", 5],
+  [58, "cebration", "2020-11-07 09:10:00", "2020-11-07 12:10:00", 9],
+];
+const sqlMeet =
+  "INSERT IGNORE INTO Meeting (meeting_no , meeting_title , starting_time , ending_time , room_no ) VALUES ?";
+insertValues(sqlMeet, meetings);
 
+//function to do query
 function doQuery(queryD) {
   db.query(queryD, (err, result) => {
     if (err) {
       throw err;
     }
-    console.log("Done");
+    console.log("Table ");
+  });
+}
+
+//function to insert values to tables
+function insertValues(sqlS, data) {
+  db.query(sqlS, [data], (err, result) => {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
   });
 }
 
--- Week1/excercises/app2.js
+++ Week1/excercises/app2.js
@@ -1,137 +1,48 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "Omran2003",
+  user: "hyfuser",
+  password: "hyfpassword",
   database: "world",
 });
 
 connection.connect();
 
 // countries Above 8M:
-connection.query(
-  "SELECT Name from country WHERE Population > 8000000",
-  function (error, results, fields) {
-    if (error) throw error;
-    const countriesAbove8M = [];
-    results.forEach((element) => {
-      countriesAbove8M.push(element.Name);
-    });
-    console.log("1-countries Above 8M: ", countriesAbove8M);
-  }
-);
 
-// countriesContainsLand:
-connection.query(
-  'SELECT Name from country WHERE Name LIKE "%land%"',
-  function (error, results, fields) {
-    if (error) throw error;
-    const countriesContainsLand = [];
-    results.forEach((element) => {
-      countriesContainsLand.push(element.Name);
-    });
-    console.log("2-countriesContainsLand: ", countriesContainsLand);
-  }
-);
-
-// citiesBetweenHalfMAnd1M,
-connection.query(
-  "SELECT Name from city WHERE Population > 500000 AND Population < 1000000",
-  function (error, results, fields) {
-    if (error) throw error;
-    const citiesBetweenHalfMAnd1M = [];
-    results.forEach((element) => {
-      citiesBetweenHalfMAnd1M.push(element.Name);
-    });
-    console.log("3-citiesBetweenHalfMAnd1M: ", citiesBetweenHalfMAnd1M);
-  }
-);
-
-// countriesInEurope,
-connection.query(
-  "SELECT Name from country WHERE Continent LIKE 'Europe'",
-  function (error, results, fields) {
-    if (error) throw error;
-    const countriesInEurope = [];
-    results.forEach((element) => {
-      countriesInEurope.push(element.Name);
-    });
-    console.log("4-countriesInEurope: ", countriesInEurope);
-  }
-);
-
-//  countriesBySurfaceArea,
-connection.query(
-  "SELECT Name from Country ORDER BY -SurfaceArea DESC",
-  function (error, results, fields) {
-    if (error) throw error;
-    const countriesBySurfaceArea = [];
-    results.forEach((element) => {
-      countriesBySurfaceArea.push(element.Name);
-    });
-    console.log(" 5-countriesBySurfaceArea: ", countriesBySurfaceArea);
-  }
-);
-
-//  cities In Netherland:,
-connection.query(
-  " SELECT Name from City WHERE countryCode='NLD'",
-  function (error, results, fields) {
-    if (error) throw error;
-    const citiesInNL = [];
-    results.forEach((element) => {
-      citiesInNL.push(element.Name);
-    });
-    console.log(" 6-citiesInNL: ", citiesInNL);
-  }
-);
-
-//  population of Rotterdam:,
-connection.query(
+const queries = [
+  "SELECT Name FROM country WHERE population > 8000000",
+  'SELECT Name FROM country WHERE name LIKE "%land%"',
+  "SELECT Name FROM city WHERE population > 500000 AND population < 1000000",
+  "SELECT Name FROM country WHERE Continent LIKE 'Europe'",
+  "SELECT Name FROM country ORDER BY -SurfaceArea DESC",
+  "SELECT Name FROM city WHERE CountryCode='NLD'",
   "SELECT Population FROM city WHERE Name = 'Rotterdam'",
-  function (error, result, fields) {
-    if (error) throw error;
+  "SELECT Name FROM country ORDER BY -SurfaceArea Limit 10",
+  "SELECT Name FROM city ORDER BY Population DESC LIMIT 10",
+  "SELECT SUM(Population) World_Population FROM country",
+];
 
-    console.log(" 7-populationRotterdam: ", result[0].Population);
-  }
-);
+const headers = [
+  "1-Countries Above 8M: ",
+  "2-Countries Contains Land in their name: ",
+  "3-Cities Between Half M And 1M: ",
+  "4-Countries In Europe: ",
+  "5-Countries Descending By Surface Area: ",
+  "6-Cities In NL: ",
+  "7-Population of Rotterdam: ",
+  "8-Top 10 Countries By Surface Area: ",
+  "9-Top 10 Cities By Population: ",
+  "10-Population Sum in world: ",
+];
+let a = 0;
 
-//  Top 10 countries by the surface area,
-connection.query(
-  " SELECT Name from Country ORDER BY -SurfaceArea Limit 10",
-  function (error, results, fields) {
-    if (error) throw error;
-    const top10CountriesBySurfaceArea = [];
-    results.forEach((element) => {
-      top10CountriesBySurfaceArea.push(element.Name);
-    });
-    console.log(
-      " 8-top10CountriesBySurfaceArea: ",
-      top10CountriesBySurfaceArea
-    );
-  }
-);
-
-//  Top 10 cities by population:,
-connection.query(
-  " SELECT Name from city ORDER BY Population DESC Limit 10",
-  function (error, results, fields) {
-    if (error) throw error;
-    const top10CitiesByPopulation = [];
-    results.forEach((element) => {
-      top10CitiesByPopulation.push(element.Name);
-    });
-    console.log("9- top10CitiesByPopulation: ", top10CitiesByPopulation);
-  }
-);
-
-//  The total population in the world
-connection.query(
-  " SELECT SUM(Population) World_Population FROM Country",
-  function (error, results, fields) {
-    if (error) throw error;
-    console.log("10- population Sum in world: ", results[0].World_Population);
-  }
-);
+queries.forEach((elem) => {
+  connection.query(elem, (err, results, fields) => {
+    if (err) throw err;
+    console.log(headers[a], results);
+    a++;
+  });
+});
 
 connection.end();
